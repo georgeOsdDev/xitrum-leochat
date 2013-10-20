da527b0431da7669984cc8b8cf228fe6ab3fba12
Takeharu.Oshida <georgeosddev@gmail.com>
2013-10-20 22:20:29 +0900

    clustering: not working now.

--- src/main/scala/leochat/action/Chat.scala
+++ src/main/scala/leochat/action/Chat.scala
@@ -1,61 +1,105 @@
 package leochat.action
 
 import akka.actor.{Actor, ActorRef, Props}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 import xitrum.{Config, Logger, SockJsActor, SockJsText, WebSocketActor, WebSocketText, WebSocketBinary, WebSocketPing, WebSocketPong}
 import xitrum.annotation.{GET, SOCKJS, WEBSOCKET}
 import xitrum.mq.{MessageQueue, QueueMessage}
 import leochat.util.LeoFS
+import glokka.Registry
 
-@GET("websocketImageChatDemo")
-class LeoChat extends AppAction {
-  def execute() {
-    respondView()
+object MessageQueManager{
+
+  val msgQueManager = Config.actorSystem.actorOf(Props[MessageQueManager], "msgQueManager")
+  def getMsgManager:ActorRef ={
+    msgQueManager
   }
+  val registry = Registry.start(Config.actorSystem, "proxy")
+  def getRegistry:ActorRef ={
+    registry
+  }
+  msgQueManager ! "init"
+  registry ! Registry.Register(Config.xitrum.port.toString, msgQueManager)
+
+//  val clusterListener = Config.actorSystem.actorOf(Props[SimpleClusterListener], name = "clusterListener")
+//  Cluster(Config.actorSystem).subscribe(clusterListener, classOf[ClusterDomainEvent])
 }
+
+//class SimpleClusterListener extends Actor {
+//  var managers = Seq[ActorRef]()
+//  def receive = {
+//    case state: CurrentClusterState ⇒
+//    case MemberUp(member) ⇒
+//      member.address.port
+//      managers = managers :+ sender
+//      managers.foreach{manager =>
+//        manager ! member
+//      }
+//    case UnreachableMember(member) ⇒
+//    case MemberRemoved(member, previousStatus) ⇒
+//    case _: ClusterDomainEvent ⇒ // ignore
+//  }
+//}
 
 case class MsgsFromQueue(msgs: Seq[String])
 case class Publish(msg:String);
 case class Subscribe(num:Int);
-
-@WEBSOCKET("leochat")
-class LeoChatActor extends WebSocketActor{
-  val manager = MessageQueManager.getManager
-  def execute() {
-
-    manager ! Subscribe(10) // read latest 10
-    context.become {
-      case MsgsFromQueue(msgs) =>
-        msgs.foreach { msg => respondWebSocketText(msg) }
-      case WebSocketText(text) =>
-        manager ! Publish(text)
-      case WebSocketBinary(bytes) =>
-      case WebSocketPing =>
-      case WebSocketPong =>
-    }
-  }
-}
-
-object MessageQueManager{
-  val manager = Config.actorSystem.actorOf(Props[MessageQueManager], "manager")
-  def getManager:ActorRef ={
-    manager
-  }
-}
+case class NewManager(name:String,actorRef:ActorRef);
 
 class MessageQueManager extends Actor with Logger{
-  val infinitie = "9999999999999".toLong
   var clients = Seq[ActorRef]()
+  var managers = Seq[ActorRef]()
+  val registry = MessageQueManager.getRegistry
+
   def receive = {
     case pub: Publish =>
-      // s3.listObjects()) they are returned in alphabetical order
-      val key = (infinitie - System.currentTimeMillis()).toString
+      // s3.listObjects() they are returned in alphabetical order
+      val key = (- System.currentTimeMillis()).toString
       LeoFS.save(key,pub.msg)
-      clients.foreach(_ ! MsgsFromQueue(Seq(pub.msg)))
+      clients.foreach{client =>
+        client ! MsgsFromQueue(Seq(pub.msg))
+      }
+      managers.foreach{manager =>
+        manager ! MsgsFromQueue(Seq(pub.msg))
+      }
     case sub: Subscribe =>
         clients = clients :+ sender
         val messages = LeoFS.readHead(sub.num)
         sender ! MsgsFromQueue(messages)
+    case msg : MsgsFromQueue => //manager to manager
+      clients.foreach{client =>
+        client ! msg
+      }
+    // TODO : who trigger this message?
+    case newManager: NewManager =>
+      managers = managers :+ newManager.actorRef
     case _ =>
       logger.error("unexpected message")
+  }
+}
+
+@GET("websocketImageChatDemo")
+class LeoChat extends AppAction {
+  def execute() {
+        respondView()
+  }
+}
+
+@WEBSOCKET("leochat")
+class LeoChatActor extends WebSocketActor{
+  val msgQueManager = MessageQueManager.getMsgManager
+  def execute() {
+
+    msgQueManager ! Subscribe(10) // read latest 10
+    context.become {
+      case MsgsFromQueue(msgs) =>
+        msgs.foreach { msg => respondWebSocketText(msg) }
+      case WebSocketText(text) =>
+        msgQueManager ! Publish(text)
+      case WebSocketBinary(bytes) =>
+      case WebSocketPing =>
+      case WebSocketPong =>
+    }
   }
 }
